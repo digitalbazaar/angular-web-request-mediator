feeff308c9f20183a93f29eb2b2a227353f2c4f4
Dave Longley <dlongley@digitalbazaar.com>
2017-08-24 11:01:49 -0400

    Add activity bar component, panel CSS, and update item chooser.
    - Show activity bar on item chooser when items are selected until
      a promise returned by `onSelect resolves.
    - Enable canceling item chooser via top-right `x` and escape key.

---
+++ activity-bar-component.js
@@ -0,0 +1,8 @@
+/*!
+ * Activity bar component.
+ *
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+export default {
+  templateUrl: 'angular-web-request-mediator/activity-bar-component.html'
+};
--- index.js
+++ index.js
@@ -4,10 +4,12 @@
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
 import angular from 'angular';
+import ActivityBarComponent from './activity-bar-component.js';
 import ItemChooserComponent from './item-chooser-component.js';
 import PermissionDialogComponent from './permission-dialog-component.js';
 
 const module = angular.module('web-request-mediator', []);
 
+module.component('wrmActivityBar', ActivityBarComponent);
 module.component('wrmItemChooser', ItemChooserComponent);
 module.component('wrmPermissionDialog', PermissionDialogComponent);
